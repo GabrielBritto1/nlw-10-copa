cd10d3dea0537ff66b7218f354d685584254b42b
Gabriel Britto <gabriel_bianchibritto@hotmail.com>
2022-11-20 12:53:24 -0300

    modificações

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function createGame(player1, hour, player2) {
     return `
     <li>
-        <img class="bandeiras" src="./assets/${player1}.svg" alt="Bandeira do ${player1}">
+        <img class="bandeiras" src="./assets/${player1}.svg" title="${player1.toUpperCase()}" alt="Bandeira do ${player1}">
         <strong>${hour}</strong>
-        <img class="bandeiras" src="./assets/${player2}.svg" alt="Bandeira da ${player2}">
+        <img class="bandeiras" src="./assets/${player2}.svg" title="${player2.toUpperCase()}" alt="Bandeira da ${player2}">
     </li>
     `
 }
+
 
 let delay = -0.4;
 function createCard(date, day, games) {
@@ -35,7 +36,7 @@ document.querySelector('#cards').innerHTML =
         createGame('mexico', '13:00', 'poland') +
         createGame('france', '16:00', 'australia')) +
 
-    createCard('23/11', 'terça',
+    createCard('23/11', 'quarta',
         createGame('morocco', '07:00', 'croatia') +
         createGame('germany', '10:00', 'japan') +
         createGame('spain', '13:00', 'costa rica') +
@@ -47,19 +48,19 @@ document.querySelector('#cards').innerHTML =
         createGame('portugal', '13:00', 'ghana') +
         createGame('brazil', '16:00', 'serbia')) +
 
-    createCard('25/11', 'quinta',
+    createCard('25/11', 'sexta',
         createGame('wales', '07:00', 'ira') +
         createGame('qatar', '10:00', 'senegal') +
         createGame('netherlands', '13:00', 'ecuador') +
         createGame('england', '16:00', 'usa')) +
 
-    createCard('26/11', 'quinta',
+    createCard('26/11', 'sábado',
         createGame('tunisia', '07:00', 'australia') +
         createGame('poland', '10:00', 'saudi arabia') +
         createGame('france', '13:00', 'denmark') +
         createGame('argentina', '16:00', 'mexico')) +
 
-    createCard('27/11', 'quinta',
+    createCard('27/11', 'domingo',
         createGame('japan', '07:00', 'costa rica') +
         createGame('belgium', '10:00', 'morocco') +
         createGame('croatia', '13:00', 'canada') +
@@ -71,13 +72,13 @@ document.querySelector('#cards').innerHTML =
         createGame('brazil', '13:00', 'switzerland') +
         createGame('portugal', '16:00', 'uruguay')) +
 
-    createCard('29/11', 'quinta',
+    createCard('29/11', 'terça',
         createGame('ecuador', '12:00', 'senegal') +
         createGame('netherlands', '12:00', 'qatar') +
         createGame('ira', '16:00', 'usa') +
         createGame('wales', '16:00', 'england')) +
 
-    createCard('30/11', 'quinta',
+    createCard('30/11', 'quarta',
         createGame('tunisia', '12:00', 'france') +
         createGame('australia', '12:00', 'denmark') +
         createGame('poland', '16:00', 'argentina') +
