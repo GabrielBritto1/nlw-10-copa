9403957bbd4390b21716642e3d46f8ddae0f27b1
Gabriel Britto <gabriel_bianchibritto@hotmail.com>
2022-11-23 01:32:01 -0300

    mudança de aplicações

--- assets/js/grupos.js
+++ assets/js/grupos.js
@@ -13,7 +13,7 @@ $.when($.ajax("https://copa22.medeiro.tech/groups")).then(function (
             <table class="table table-hover table-condensed">
                 <tbody>
                     <tr>
-                        <td><img class="logoQatar" src="/assets/img/${element2.alternateName.toLowerCase()}.svg" alt="Logo da copa 2022" width="20px"></td>
+                        <td><img class="logoQatar" src="/assets/img/${element2.alternateName.toLowerCase()}.svg" alt="Bandeira do ${element2.alternateName}" width="20px"></td>
                         <td class="text-center">${element2.alternateName.toUpperCase()}</td>
                         <td class="text-right">${element2.points}</td>
                     </tr>
--- assets/js/partidas.js
+++ assets/js/partidas.js
@@ -1,34 +1,59 @@
 // LISTAGEM DE PARTIDAS
-$.when($.ajax("https://copa22.medeiro.tech/matches")).then(function (data) {
-    let status = ``;
-    let dia = ``;
 
-    for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        status = ``;
-        dia = ``;
+//criando função
+function carregaPartidas() {
 
-        if (element.status == "completed") {
-            status = `bg-primary`;
-        } else if (element.status == "in_progress") {
-            status = `bg-warning`;
-        }
+    // consulta ajax no endpoint
+    $.when($.ajax("https://copa22.medeiro.tech/matches")).then(function (data) {
 
-        let myDate = element.date.split("T");
-        let myYear = myDate[0].split("-")[0];
-        let myMonth = myDate[0].split("-")[1];
-        let myDay = myDate[0].split("-")[2];
-        let myTime = myDate[1].split(":")[0];
-        myTime = parseInt(myTime) - 3;
+        // inicializando as variáveis
+        let status = ``;
+        let dia = ``;
 
-        $("#container").append(`
+        // iterando o resultado obtido através da api
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            status = ``;
+            dia = ``;
+
+            // condicionais para mudar a cor do background
+            if (element.status == "completed") {
+                status = `bg-primary`;
+            } else if (element.status == "in_progress") {
+                status = `bg-warning`;
+            }
+
+            // 2022-11-23T10:00:00.000Z
+
+            // [0]// 2022-11-23
+            // [1]// 10:00:00.000Z
+
+            let myDate = element.date.split("T");
+            let myYear = myDate[0].split("-")[0];
+            let myMonth = myDate[0].split("-")[1];
+            let myDay = myDate[0].split("-")[2];
+            let myTime = myDate[1].split(":")[0];
+            myTime = parseInt(myTime) - 3;
+
+
+            let time1 = element.homeTeam.name;
+            let time2 = element.awayTeam.name;
+            // if (element.winner == time1) {
+            //     time1 = `${time1} ganhou`
+            // } else if (element.winner == time2) {
+            //     time2 = `${time2} ganhou`
+            // }
+
+            $("#container").append(`
                 <div class="col-md-4">
 
                     <div class="card mb-3 text-center ${status}">
                         <div class="card-header">
-                            ${element.homeTeam.name}: <span class="badge badge-light">${element.homeTeam.goals}</span> 
-                            x 
-                            ${element.awayTeam.name}: <span class="badge badge-light">${element.awayTeam.goals}</span>
+                        <img class="logoQatar" src="/assets/img/${element.homeTeam.name.toLowerCase()}.svg" alt="Bandeira do ${element.homeTeam.name}" width="20px">
+                            ${time1}: <span class="badge badge-light">${element.homeTeam.goals}</span> 
+                            x
+                        <img class="logoQatar" src="/assets/img/${element.awayTeam.name.toLowerCase()}.svg" alt="Bandeira do ${element.awayTeam.name}" width="20px">
+                            ${time2}: <span class="badge badge-light">${element.awayTeam.goals}</span>
                         </div>
                         <ul class="list-group list-group-flush">
                             ${myDay}/${myMonth}/${myYear} as ${myTime}:00
@@ -37,5 +62,8 @@ $.when($.ajax("https://copa22.medeiro.tech/matches")).then(function (data) {
 
                 </div>
             `);
-    }
-});
+        }
+    });
+}
+
+carregaPartidas();
