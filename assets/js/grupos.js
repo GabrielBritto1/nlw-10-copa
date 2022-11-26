96ddf7a62ce5caca7f92363a080082cbffe4958b
João Filipi <jf.britto@hotmail.com>
2022-11-26 02:10:45 -0300

    tabela

--- assets/js/grupos.js
+++ assets/js/grupos.js
@@ -20,12 +20,12 @@ $(document).ready( function () {
                     <tr class="${class_tr}">
                         <td class="text-left"><img class="img" src="./assets/img/flags/${element2.name.toLowerCase()}.svg" alt="Bandeira do ${element2.name}" width="20px"> &nbsp; ${element2.country}</td>
                         <td>${element2.group_points}</td>
-                        <td>${element2.wins}</td>
-                        <td>${element2.draws}</td>
-                        <td>${element2.losses}</td>
+                        <td class="d-none">${element2.wins}</td>
+                        <td class="d-none">${element2.draws}</td>
+                        <td class="d-none">${element2.losses}</td>
                         <td>${element2.games_played}</td>
-                        <td>${element2.goals_for}</td>
-                        <td>${element2.goals_against}</td>
+                        <td class="d-none">${element2.goals_for}</td>
+                        <td class="d-none">${element2.goals_against}</td>
                         <td>${element2.goal_differential}</td>
                     </tr>
                     `
@@ -39,14 +39,14 @@ $(document).ready( function () {
                                 <table class="table" id="table-${index}">
                                     <thead>
                                         <tr>
-                                            <th>Grupo ${element.letter}</th>
+                                            <th><span class="badge badge-secondary">Grupo ${element.letter}</span></th>
                                             <th>PT</th>
-                                            <th>V</th>
-                                            <th>E</th>
-                                            <th>D</th>
+                                            <th class="d-none">V</th>
+                                            <th class="d-none">E</th>
+                                            <th class="d-none">D</th>
                                             <th>J</th>
-                                            <th>GP</th>
-                                            <th>GC</th>
+                                            <th class="d-none">GP</th>
+                                            <th class="d-none">GC</th>
                                             <th>SG</th>
                                         </tr>
                                     </thead>
@@ -76,14 +76,14 @@ $(document).ready( function () {
     // });
 
     setTimeout(() => {
-        $('#table-0').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-1').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-2').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-3').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-4').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-5').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-6').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
-        $('#table-7').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc']]});
+        $('#table-0').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-1').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-2').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-3').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-4').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-5').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-6').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
+        $('#table-7').DataTable({searching: false, paging: false, info: false, order: [[1, 'desc'], [8, 'desc']]});
     }, 500);
 
 } );
