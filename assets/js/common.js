51d97fcf0a130f6929165550b4221b42da8e7f9a
João Filipi <jf.britto@hotmail.com>
2022-11-26 01:39:29 -0300

    nome arquivo

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
 $("#nav").html(`
     <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-        <a class="navbar-brand" href="./acontecendo.html"><img src="./assets/img/logo.png" alt="Logo Copa 2022" width="100px"></a>
+        <a class="navbar-brand" href="./index.html"><img src="./assets/img/logo.png" alt="Logo Copa 2022" width="100px"></a>
         <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
